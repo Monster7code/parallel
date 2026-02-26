69efb8092deb2d00a4d144a5ea092fe0bd84fe41
Monster7code <68667310+Monster7code@users.noreply.github.com>
2026-02-26 16:55:28 +0500

    Add files via upload

---
+++ helow1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "omp.h"
+
+int main()
+{
+#pragma omp parallel
+	{
+		int nthread = omp_get_num_threads();
+		int thread_id = omp_get_thread_num();
+#pragma omp critical
+		std::cout << "Hello, world from thread "
+			<< thread_id << " of "
+			<< nthread << std::endl;
+	}
+}
